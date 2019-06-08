0e5b157a135372d465b14a3fe4149d0c1e27f223
AntyfrizZz <iavor.milinov@gmail.com>
2019-06-08 22:34:53 +0300

    Add router

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import * as SBoard from "./components/styled/Board";
 import { CustomButton } from "./components/shared/CustomButton";
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Game } from "./components/game/game";
+import { HomeScreen } from "./components/homeScreen/homeScreen";
+
+import { NotFound } from "./components/notFound/NotFound";
 
 const App: React.FC = () => {
   return (
-    <SBoard.board>
-      <CustomButton text="New game" />
-    </SBoard.board>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/game">Game</Link>
+          </li>
+        </ul>
+      </div>
+      <Switch>
+        <Route exact path="/" component={HomeScreen} />
+        <Route path="/game" component={Game} />
+        <Route component={NotFound} />
+      </Switch>
+      <div>Footer</div>
+    </Router>
   );
 };
 
---
+++ hangman/src/components/homeScreen/homeScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const HomeScreen: React.FC = () => {
+  return (
+    <div>home screen</div>
+  );
+};
