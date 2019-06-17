5c66ceae343addd88feaebfc33db7bb826e8f9a2
AntyfrizZz <iavor.milinov@gmail.com>
2019-06-17 21:43:49 +0300

    Change the styling for the letter cards

--- hangman/src/components/styled/GameScreenStyled.ts
+++ hangman/src/components/styled/GameScreenStyled.ts
@@ -2,12 +2,24 @@ import styled, { css } from "styled-components";
 
 export const LetterCard = styled.div<{ disabled: boolean }>`
   width: 20px;
-  height: 40px;
+  height: 20px;
+  padding: 5px;
+  margin: 5px;
+  line-height: 20px;
+  text-align: center;
   cursor: pointer;
   border: 1px solid black;
-  ${(props) =>
+  -webkit-box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.75);
+  ${props =>
     props.disabled &&
     css`
       cursor: not-allowed;
+      color: grey;
+      border-color: grey;
+      -webkit-box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.25);
+      -moz-box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.25);
+      box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.25);
     `};
 `;
